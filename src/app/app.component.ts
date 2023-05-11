a90d0940e79ded96131881a4195203a6c2a51e18
Jelena Basaric <jbasaric83@gmail.com>
2023-05-11 11:31:44 +0200

    styling output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles:['.whiteText{color:white;}']
+  //styles:['.whiteText{color:white;background:blue;}'] //moze i ovako, onda bez ngStyle
 })
 export class AppComponent {
   title = 'thirdHomework';
   logs :number[]=[];
+  //logs :Date[] =[];
   showPassword=false;
   onDisplayButtonClick(){
     this.showPassword=!this.showPassword;
     this.logs.push(this.logs.length+1);
+    //this.logs.push (new Date);
+    // ali mora da se promeni inicalizacija niza
   }
 }
